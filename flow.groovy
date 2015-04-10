ff94ad249e0fca54e19e7eec964844ce622e9cf7
Olve Sæther Hansen <olve@vimond.com>
2015-04-10 12:47:03 +0200

    fix

--- flow.groovy
+++ flow.groovy
@@ -4,6 +4,6 @@ node('docker-image-builder'){
 
   def docker = new com.vimond.workflow.Docker()
 
-    docker.buildTagPush('vimond.artifactoryonline.com/micros-baseimage')
+  docker.buildTagPush('micros-baseimage')
     
   }
