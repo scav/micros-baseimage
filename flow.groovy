ea596435f53e407fd844bd5b061d76c1f5787c74
Olve Sæther Hansen <olve@vimond.com>
2015-04-13 16:17:51 +0200

    fix

--- flow.groovy
+++ flow.groovy
@@ -1,9 +1,12 @@
 stage 'inital'
 
 node('docker-image-builder'){
-
+  git changelog: true, url: 'git@github.com:vimond/vimond-base.git', credentialsId: 'github_jenkins'
   def docker = new com.vimond.workflow.Docker()
 
   docker.buildTagPush('micros-baseimage')
     
   }
+
+  stage 'inital'
+
