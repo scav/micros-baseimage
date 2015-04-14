5e273a6cdbd251363bb365ceecbca5b59775e1cf
Olve Sæther Hansen <olve@vimond.com>
2015-04-14 11:00:28 +0200

    Added support for maven built services (added jars from ./target/*.jar

--- flow.groovy
+++ flow.groovy
@@ -1,7 +1,7 @@
 stage 'inital'
 
 node('docker-image-builder'){
-  git changelog: true, url: 'git@github.com:vimond/vimond-base.git', credentialsId: 'github_jenkins'
+  git changelog: true, url: 'git@github.com:vimond/micros-baseimage.git', credentialsId: 'github_jenkins'
   def docker = new com.vimond.workflow.Docker()
 
   docker.buildTagPush('micros-baseimage')
