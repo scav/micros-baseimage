623cea2b146a76f34ab490f19e953bfd5cdec910
Christoffer Eide <cei@vimond.com>
2015-12-11 16:10:38 +0100

    Fixed issue where /sbin/setuser was called two times, resulting in the error:
    Traceback (most recent call last):
     File "/sbin/setuser", line 17, in <module>
       os.initgroups(username, user.pw_gid)
    PermissionError: [Errno 1] Operation not permitted

--- docker-service-startup-command.sh
+++ docker-service-startup-command.sh
@@ -2,6 +2,6 @@
 
 if [ "$SERVICE_CMD" != "server" ]
 then
- 	exec /sbin/setuser $SERVICE_NAME /etc/service/${SERVICE_NAME}/run
+ 	exec /etc/service/${SERVICE_NAME}/run
 fi
 echo server > /etc/container_environment/SERVICE_CMD
